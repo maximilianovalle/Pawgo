9570ae097f1f96ef57effd8359199c61c67c6701
maximilianovalle <109977231+maximilianovalle@users.noreply.github.com>
2025-06-01 16:01:58 -0500

    Setup backend and frontend

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
